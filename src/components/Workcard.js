a4ddc03ce710a802d4b4de38850d452579ac9dd9
RohithMedisetty <80573302+RohithMedisetty@users.noreply.github.com>
2023-08-23 19:10:17 -0500

    newfeature

--- src/components/Workcard.js
+++ src/components/Workcard.js
@@ -3,15 +3,19 @@ import React from 'react'
 import { NavLink } from "react-router-dom";
 
 const Workcard = (props) => {
+  const isViewEmpty = props.view
+  console.log(isViewEmpty)
   return (
     <div className="project-card">
       
         <img src={props.imgsrc} alt=""/>
                 <h2 className="project-title">{props.title}</h2>
+                
                 <div className="pro-details">
                     <p>{props.text}</p>
                     <div className="pro-btns">
-                        <NavLink to={props.view} className="btn">View</NavLink>
+                        <NavLink to={props.view} 
+                        className="btn">View</NavLink>
                         <NavLink to={props.source} className="btn">Source</NavLink>
                     </div>
                 </div>
